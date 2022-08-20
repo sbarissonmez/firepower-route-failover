ed96b85963cad25b7445eaa3b80fb9d9508aab25
Baris <s.barissonmez@gmail.com>
2022-08-20 19:19:17 +0300

    Update routefailover.py

--- routefailover.py
+++ routefailover.py
@@ -15,3 +15,22 @@ MAX_LATENCY = options['max_latency']
 MAX_LOSS = options['max_loss']
 #######
 
+
+def doPing():
+    # Run ping against target destination
+    try:
+        print("Checking loss/latency to " + DEFAULT_DESTINATION)
+        result = ping(DEFAULT_DESTINATION, size=2, count=PING_COUNT,
+                      verbose=True)
+    except PermissionError:
+        # must have privileges to ping
+        print("Error: User not root! Cannot open socket")
+        sys.exit(0)
+    sleep(1)
+    print("Calculating results...")
+    loss = calculateLoss(result)
+    print("AVG response time: " + str(result.rtt_avg_ms) +
+          "ms. Loss = " + str(loss) + "%")
+    print("Target response time: <" + str(MAX_LATENCY) +
+          "ms. Target Loss: <" + str(MAX_LOSS))
+    return result.rtt_avg_ms, loss
